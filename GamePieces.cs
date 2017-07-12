dd24055cc8ee4b758c51279c4f9d10fb3d49782f
DavidRHid <davidrhidary@gmail.com>
2017-07-12 12:06:26 -0400

    Add files via upload
    Here are 3 of my current scripts using c#.

---
+++ GamePieces.cs
@@ -0,0 +1,248 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePieces : MonoBehaviour {
+    public GameScript GS;
+    public Vector3 Position;
+    public Vector2 Location;
+    public bool white;
+
+    // Use this for initialization
+    void Start() {
+        GS = GameObject.Find("Main Camera").GetComponent<GameScript>();
+    }
+
+    // Update is called once per frame
+    private void OnMouseOver()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            if (white == true)
+            {
+                if (Input.GetMouseButton(0))
+                {
+                    if (GS.loctaken[(int)Location.x - GS.HighDice, i] == false)
+                    {
+                        MoveHighBack();
+                        return;
+
+                    }
+                }
+                else if (Input.GetMouseButton(1))
+                {
+                    if (GS.loctaken[(int)Location.x - GS.LowDice, i] == false)
+                    {
+                        MoveLowBack();
+                        return;
+
+                    }
+                }
+            }
+
+            else if (white == false)
+            {
+
+                if (Input.GetMouseButton(0))
+                {
+                    if (GS.loctaken[(int)Location.x + GS.HighDice, i] == false)
+                    {
+
+                        MoveHigh();
+                        return;
+                    }
+                }
+
+                else if (Input.GetMouseButton(1))
+                {
+                    if (GS.loctaken[(int)Location.x + GS.LowDice, i] == false)
+                    {
+                        MoveLow();
+                        return;
+                    }
+                }
+        }
+    }
+        if (white == true) {
+
+        }
+    }
+    
+
+    public void MoveHigh()
+    {
+        // moves piece using high roll to high spot
+        if (GS.loctaken[GS.HighDice + (int)Location.x, 1] == false)
+        {
+            transform.position = (GS.locations[GS.HighDice + (int)Location.x, 1]);
+            GS.loctaken[GS.HighDice + (int)Location.x, 1] = true;
+            GS.loctaken[(int)Location.x, (int)Location.y] = false;
+            Location = new Vector2(GS.HighDice + (int)Location.x, 1);
+        }
+        else if (GS.loctaken[GS.HighDice + (int)Location.x, 2] == false)
+        {
+            transform.position = (GS.locations[GS.HighDice + (int)Location.x, 2]);
+            GS.loctaken[GS.HighDice + (int)Location.x, 2] = true;
+            GS.loctaken[(int)Location.x, (int)Location.y] = false;
+            Location = new Vector2(GS.HighDice + (int)Location.x, 2);
+        }
+        else if (GS.loctaken[GS.HighDice + (int)Location.x, 3] == false)
+        {
+            transform.position = (GS.locations[GS.HighDice + (int)Location.x, 3]);
+            GS.loctaken[GS.HighDice + (int)Location.x, 3] = true;
+            GS.loctaken[(int)Location.x, (int)Location.y] = false;
+            Location = new Vector2(GS.HighDice + (int)Location.x, 3);
+        }
+        else if (GS.loctaken[GS.HighDice + (int)Location.x, 4] == false)
+        {
+            transform.position = (GS.locations[GS.HighDice + (int)Location.x, 4]);
+            GS.loctaken[GS.HighDice + (int)Location.x, 4] = true;
+            GS.loctaken[(int)Location.x, (int)Location.y] = false;
+            Location = new Vector2(GS.HighDice + (int)Location.x, 4);
+        }
+        else if (GS.loctaken[GS.HighDice + (int)Location.x, 5] == false)
+        {
+            transform.position = (GS.locations[GS.HighDice + (int)Location.x, 5]);
+            GS.loctaken[GS.HighDice + (int)Location.x, 5] = true;
+            GS.loctaken[(int)Location.x, (int)Location.y] = false;
+            Location = new Vector2(GS.HighDice + (int)Location.x, 5);
+        }
+        else
+        {
+            Debug.Log("B H Over");
+            return;
+        }
+    }
+
+       public void MoveHighBack(){
+            if (GS.loctaken[(int)Location.x - GS.HighDice, 1] == false)
+            {
+                transform.position = (GS.locations[(int)Location.x - GS.HighDice, 1]);
+                GS.loctaken[(int)Location.x - GS.HighDice, 1] = true;
+                GS.loctaken[(int)Location.x, (int)Location.y] = false;
+                Location = new Vector2( (int)Location.x - GS.HighDice, 1);
+            }
+            else if (GS.loctaken[(int)Location.x - GS.HighDice, 2] == false)
+            {
+                transform.position = (GS.locations[(int)Location.x - GS.HighDice, 2]);
+                GS.loctaken[(int)Location.x - GS.HighDice, 2] = true;
+                GS.loctaken[(int)Location.x, (int)Location.y] = false;
+                Location = new Vector2( (int)Location.x - GS.HighDice, 2);
+            }
+            else if (GS.loctaken[(int)Location.x - GS.HighDice, 3] == false)
+            {
+                transform.position = (GS.locations[(int)Location.x - GS.HighDice, 3]);
+                GS.loctaken[(int)Location.x - GS.HighDice, 3] = true;
+                GS.loctaken[(int)Location.x, (int)Location.y] = false;
+                Location = new Vector2((int)Location.x - GS.HighDice, 3);
+            }
+            else if (GS.loctaken[(int)Location.x - GS.HighDice, 4] == false)
+            {
+                transform.position = (GS.locations[(int)Location.x - GS.HighDice, 4]);
+                GS.loctaken[(int)Location.x - GS.HighDice, 4] = true;
+                GS.loctaken[(int)Location.x, (int)Location.y] = false;
+                Location = new Vector2((int)Location.x - GS.HighDice, 4);
+            }
+            else if (GS.loctaken[(int)Location.x - GS.HighDice, 5] == false)
+            {
+                transform.position = (GS.locations[(int)Location.x - GS.HighDice, 5]);
+                GS.loctaken[(int)Location.x - GS.HighDice, 5] = true;
+                GS.loctaken[(int)Location.x, (int)Location.y] = false;
+                Location = new Vector2(Location.x - GS.HighDice, 5);
+            }
+        else
+        {
+            Debug.Log("W H Over");
+            return;
+        }
+    }
+    public void MoveLow()
+    {
+        if (GS.loctaken[GS.LowDice + (int)Location.x, 1] == false)
+        {
+            transform.position = (GS.locations[GS.LowDice + (int)Location.x, 1]);
+            GS.loctaken[GS.LowDice + (int)Location.x, 1] = true;
+            GS.loctaken[(int)Location.x, (int)Location.y] = false;
+            Location = new Vector2(GS.LowDice + (int)Location.x, 1);
+        }
+        else if (GS.loctaken[GS.LowDice + (int)Location.x, 2] == false)
+        {
+            transform.position = (GS.locations[GS.LowDice + (int)Location.x, 2]);
+            GS.loctaken[GS.LowDice + (int)Location.x, 2] = true;
+            GS.loctaken[(int)Location.x, (int)Location.y] = false;
+            Location = new Vector2(GS.LowDice + (int)Location.x, 2);
+        }
+        else if (GS.loctaken[GS.LowDice + (int)Location.x, 3] == false)
+        {
+            transform.position = (GS.locations[GS.LowDice + (int)Location.x, 3]);
+            GS.loctaken[GS.LowDice + (int)Location.x, 3] = true;
+            GS.loctaken[(int)Location.x, (int)Location.y] = false;
+            Location = new Vector2(GS.LowDice + (int)Location.x, 3);
+        }
+        else if (GS.loctaken[GS.LowDice + (int)Location.x, 4] == false)
+        {
+            transform.position = (GS.locations[GS.LowDice + (int)Location.x, 4]);
+            GS.loctaken[GS.LowDice + (int)Location.x, 4] = true;
+            GS.loctaken[(int)Location.x, (int)Location.y] = false;
+            Location = new Vector2(GS.LowDice + (int)Location.x, 4);
+        }
+        else if (GS.loctaken[GS.LowDice + (int)Location.x, 5] == false)
+        {
+            transform.position = (GS.locations[GS.LowDice + (int)Location.x, 5]);
+            GS.loctaken[GS.LowDice + (int)Location.x, 5] = true;
+            GS.loctaken[(int)Location.x, (int)Location.y] = false;
+            Location = new Vector2(GS.LowDice + (int)Location.x, 5);
+        }
+        else
+        {
+            Debug.Log("B B Over");
+            return;
+        }
+    }
+     public void MoveLowBack()
+    {
+        if (GS.loctaken[(int)Location.x - GS.LowDice, 1] == false)
+        {
+            transform.position = (GS.locations[(int)Location.x - GS.LowDice, 1]);
+            GS.loctaken[(int)Location.x - GS.LowDice, 1] = true;
+            GS.loctaken[(int)Location.x, (int)Location.y] = false;
+            Location = new Vector2((int)Location.x - GS.LowDice, 1);
+        }
+        else if (GS.loctaken[(int)Location.x - GS.LowDice, 2] == false)
+        {
+            transform.position = (GS.locations[(int)Location.x - GS.LowDice, 2]);
+            GS.loctaken[(int)Location.x - GS.LowDice, 2] = true;
+            GS.loctaken[(int)Location.x, (int)Location.y] = false;
+            Location = new Vector2((int)Location.x - GS.LowDice, 2);
+        }
+        else if (GS.loctaken[(int)Location.x - GS.LowDice, 3] == false)
+        {
+            transform.position = (GS.locations[(int)Location.x - GS.LowDice, 3]);
+            GS.loctaken[(int)Location.x - GS.LowDice, 3] = true;
+            GS.loctaken[(int)Location.x, (int)Location.y] = false;
+            Location = new Vector2((int)Location.x - GS.LowDice, 3);
+        }
+        else if (GS.loctaken[(int)Location.x - GS.LowDice, 4] == false)
+        {
+            transform.position = (GS.locations[(int)Location.x - GS.LowDice, 4]);
+            GS.loctaken[(int)Location.x - GS.LowDice, 4] = true;
+            GS.loctaken[(int)Location.x, (int)Location.y] = false;
+            Location = new Vector2((int)Location.x - GS.LowDice, 4);
+        }
+        else if (GS.loctaken[(int)Location.x - GS.LowDice, 5] == false)
+        {
+            transform.position = (GS.locations[(int)Location.x - GS.LowDice, 5]);
+            GS.loctaken[(int)Location.x - GS.LowDice, 5] = true;
+            GS.loctaken[(int)Location.x, (int)Location.y] = false;
+            Location = new Vector2(Location.x - GS.LowDice, 5);
+        }
+        else
+        {
+            Debug.Log("B W Over");
+            return;
+        }
+    }
+
+}
+
+
---
+++ GameScript.cs
@@ -0,0 +1,195 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameScript : MonoBehaviour
+{
+    //Variables
+   public Vector3[,] locations = new Vector3[24, 6];
+   public GameObject[] p1pieces = new GameObject[16];
+   public GameObject[] p2pieces = new GameObject[16];
+    public bool[,] loctaken = new bool[25, 6];
+    public Button RollDiceButton;
+    public Canvas MyCanvas;
+    public GameObject Player1Piece;
+    public GameObject Player2Piece;
+   public int DieOne;
+   public int DieTwo;
+  public  int HighDice;
+    public int LowDice;
+    // Use this for initialization
+    void Start()
+    {
+        //Player1 Home
+        for (int i = 0; i < 6; i++)
+        {
+
+            locations[i, 1] = new Vector3(-34f + (i * 5.8f), 1.0f, 27f);
+            locations[i, 2] = new Vector3(-34f + (i * 5.8f), 1.0f, 22.75f);
+            locations[i, 3] = new Vector3(-34f + (i * 5.8f), 1.0f, 18.5f);
+            locations[i, 4] = new Vector3(-34f + (i * 5.8f), 1.0f, 14.25f);
+            locations[i, 5] = new Vector3(-34f + (i * 5.8f), 1.0f, 10f);
+
+        }
+
+
+        //Plyer One Outer Home
+        for (int i = 6; i < 12; i++)
+        {
+
+            locations[i, 1] = new Vector3(-30 + (i * 5.8f), 1.0f, 27f);
+            locations[i, 2] = new Vector3(-30 + (i * 5.8f), 1.0f, 22.75f);
+            locations[i, 3] = new Vector3(-30 + (i * 5.8f), 1.0f, 18.5f);
+            locations[i, 4] = new Vector3(-30 + (i * 5.8f), 1.0f, 14.25f);
+            locations[i, 5] = new Vector3(-30 + (i * 5.8f), 1.0f, 10f);
+        }
+
+        //Player Two Outer Home
+        for (int i = 12; i < 18; i++)
+        {
+
+            locations[i, 1] = new Vector3(104 - (i * 5.8f), 1.0f, -27f);
+            locations[i, 2] = new Vector3(104 - (i * 5.8f), 1.0f, -22.75f);
+            locations[i, 3] = new Vector3(104 - (i * 5.8f), 1.0f, -18.5f);
+            locations[i, 4] = new Vector3(104 - (i * 5.8f), 1.0f, -14.25f);
+            locations[i, 5] = new Vector3(104 - (i * 5.8f), 1.0f, -10f);
+        }
+
+        //Player Two Home
+        for (int i = 18; i < 24; i++)
+        {
+
+            locations[i, 1] = new Vector3(100 - (i * 5.8f), 1.0f, -27f);
+            locations[i, 2] = new Vector3(100 - (i * 5.8f), 1.0f, -22.75f);
+            locations[i, 3] = new Vector3(100 - (i * 5.8f), 1.0f, -18.5f);
+            locations[i, 4] = new Vector3(100 - (i * 5.8f), 1.0f, -14.25f);
+            locations[i, 5] = new Vector3(100 - (i * 5.8f), 1.0f, -10f);
+        }
+        for (int i = 0; i < 5; i++)
+        {
+            // Start Position W
+            p1pieces[i] = Instantiate(Player1Piece, locations[5, i + 1], Quaternion.identity);
+            p1pieces[i].GetComponent<GamePieces>().Position = locations[5, i + 1];
+            loctaken[5, i + 1] = true;
+            p1pieces[i].GetComponent<GamePieces>().Location = new Vector2(5, i + 1);
+            
+
+        }
+        for (int i = 0; i < 25; i++) {
+            for (int j = 0; j < 6; j++) {
+                loctaken [i, j] = false;
+            }
+        }
+        for (int i = 5; i < 8; i++)
+        {
+            p1pieces[i] = Instantiate(Player1Piece, locations[7, i - 4], Quaternion.identity);
+            p1pieces[i].GetComponent<GamePieces>().Position = locations[7, i - 4];
+            loctaken[7, i - 4] = true;
+            p1pieces[i].GetComponent<GamePieces>().Location = new Vector2(7, i - 4);
+        }
+
+        for (int i = 8; i < 13; i++)
+        {
+            p1pieces[i] = Instantiate(Player1Piece, locations[12, i - 7], Quaternion.identity);
+            p1pieces[i].GetComponent<GamePieces>().Position = locations[12, i - 7];
+            loctaken[12, i - 7] = true;
+            p1pieces[i].GetComponent<GamePieces>().Location = new Vector2(12, i - 7);
+        }
+        for (int i = 13; i < 15; i++)
+        {
+            p1pieces[i] = Instantiate(Player1Piece, locations[23, i - 12], Quaternion.identity);
+            p1pieces[i].GetComponent<GamePieces>().Position = locations[23, i - 12];
+            loctaken[23, i -12] = true;
+            p1pieces[i].GetComponent<GamePieces>().Location = new Vector2(23, i - 12);
+        }
+        // Start Position B
+        for (int i = 13; i < 15; i++)
+        {
+            p2pieces[i] = Instantiate(Player2Piece, locations[0, i - 12], Quaternion.identity);
+            p2pieces[i].GetComponent<GamePieces>().Position = locations[0, i - 12];
+            loctaken[0, i - 12] = true;
+            p2pieces[i].GetComponent<GamePieces>().Location = new Vector2(0, i - 12);
+
+        }
+        for (int i = 8; i < 13; i++)
+        {
+            p2pieces[i] = Instantiate(Player2Piece, locations[11, i - 7], Quaternion.identity);
+            p2pieces[i].GetComponent<GamePieces>().Position = locations[11, i - 7];
+            loctaken[11, i - 7] = true;
+            p2pieces[i].GetComponent<GamePieces>().Location = new Vector2(11, i - 7);
+        }
+
+        for (int i = 5; i < 8; i++)
+        {
+            p2pieces[i] = Instantiate(Player2Piece, locations[16, i - 4], Quaternion.identity);
+            p2pieces[i].GetComponent<GamePieces>().Position = locations[16, i - 4];
+            loctaken[16, i - 4] = true;
+            p2pieces[i].GetComponent<GamePieces>().Location = new Vector2(16, i - 4);
+        }
+        for (int i = 0; i < 5; i++)
+        {
+            p2pieces[i] = Instantiate(Player2Piece, locations[18, i + 1], Quaternion.identity);
+            p2pieces[i].GetComponent<GamePieces>().Position = locations[18, i + 1];
+            loctaken[18, i + 1] = true;
+            p2pieces[i].GetComponent<GamePieces>().Location = new Vector2(18, i + 1);
+        }
+
+
+        Button btn =  Instantiate(RollDiceButton, new Vector3(0f, 10f, 0f),
+            Quaternion.AngleAxis(90, Vector3.right) * (Quaternion.AngleAxis(180, Vector3.forward)));
+        btn.transform.SetParent(MyCanvas.transform);
+        btn.onClick.AddListener(VarDice);
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(DieOne > DieTwo)
+        {
+             HighDice = DieOne;
+             LowDice = DieTwo;
+        }
+        else
+        {
+            HighDice = DieTwo;
+            LowDice = DieOne;
+        }
+    }
+
+    void VarDice()
+    {
+        DieOne = RollDice();
+        DieTwo = RollDice();
+        Debug.Log(DieOne);
+        Debug.Log(DieTwo);
+    }
+    int First() {
+        if (DieOne > DieTwo) {
+            return 1;
+        }
+
+        else if (DieOne == DieTwo) {
+            return First();
+        }
+        else
+        {
+            return 0;
+        }
+ 
+    }
+
+    int RollDice()
+    {
+        return Random.Range(1, 7);
+    }
+}
+/* if (WRow5 is cliked) {
+            highlight WRow5 +DieOne;
+            highlight WRow5 +DieTwo;
+            highlight WRow5 +DieTwo + DieOne;
+        }
+        if (WRow5 + DieOne is clicked) {
+            transform.position(WRow5 Top key to WRow5 +DieOne Top Key);
+        }*/
